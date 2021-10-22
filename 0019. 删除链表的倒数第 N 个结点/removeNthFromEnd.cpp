53b0c0b3d8c2beb08097fa64278ac934e2e4e4aa
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2021-10-22 16:21:44 +0800

    Create removeNthFromEnd.cpp

---
+++ 0019. 删除链表的倒数第 N 个结点/removeNthFromEnd.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *p1 = head;
+        while (n--) p1 = p1->next; 
+        ListNode *dummy = new ListNode(-1, head), *p2 = dummy;
+        while (p1 != nullptr) {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        p2->next = p2->next->next;
+        return dummy->next;
+    }
+};
