858c2e02dd02a6cbaf349010cfe657c744a5bc28
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2021-10-22 15:54:28 +0800

    Create mergeKLists.cpp

---
+++ 0023. 合并K个升序链表/mergeKLists.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode *ans = nullptr;
+        for (size_t i = 0; i < lists.size(); ++i) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p = dummy, *p1 = l1, *p2 = l2;
+        while(p1 != nullptr && p2 != nullptr) {
+            if (p1->val <= p2->val) {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            else {
+                p->next = p2;
+                p2 = p2->next;
+            }
+            p = p->next;
+        }
+        if (p1 != nullptr) {
+            p->next = p1;
+        }
+        else {
+            p->next = p2;
+        }
+        return dummy->next;
+    }
+};
